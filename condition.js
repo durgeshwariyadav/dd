8052c8d6ad89feaa43d9dc82984156aa6a46559a
durgeshwariyadav <163391069+durgeshwariyadav@users.noreply.github.com>
2024-03-14 14:36:46 +0530

    Add files via upload

---
+++ condition.js
@@ -0,0 +1,375 @@
+// const input=require("readline-sync");
+//  let a= input.questionInt("enter a");
+//  let b=input.questionInt("enterb");
+// if(a>b){
+//     console.log(a);
+// }
+// else{
+//     console.log(b);
+// }
+
+// const input =require("readline-sync");
+// let num =input.questionInt("enter a");
+// if(num%2 ==0) {
+//     console.log("Even");
+// } else{
+//     console.log("Odd");
+// }
+
+// const input =require("readline-sync");
+// let a=input.questionInt("enter n");
+// if(a==0){
+//     console.log("a,zero");
+// } else
+// if(a>0){
+//     console.log("a,postive");
+//  } else {
+//     console.log(" a,negative");
+// }
+
+
+// const input=require("readline-sync");
+// let n=input.questionInt("enter n");
+// if(n%2==0){
+//     console.log(n);
+// }else{
+//     n=n*2
+//     console.log(n);
+// }
+
+
+
+// const input=require("readline-sync");
+// let a=input.questionInt("enter a");
+// let b=input.questionInt("enter b");
+// let c=input.questionInt("enterc");
+// if(a>b){
+//     max=a;
+//     min=b;
+// }else{
+//     max=b;
+//     min=a;
+// }
+// if(b>c) {
+//     max1=b;
+//     min1=c;
+// }else{
+//     max1=c;
+//     min1=b;
+// }
+// if(max>max1) {
+//     console.log(max);
+// }else{
+//     console.log(max1) ;
+// }                                                                               
+
+
+
+// const input=require("readline-sync");
+// let a=input.questionInt("entera");
+// let b=input.questionInt("enterb");
+// let c=input.questionInt("enterc");
+// let d=input.questionInt("enterd");
+// if(a>b){
+//     max=a;
+//     min=b;
+// }else{
+//     max=b;
+//     min=a;
+// }
+//     if(c>d){
+//         max1=c;
+//         min1=d;
+//     }else{
+//         max1=d;
+//         min1=c;
+//     }
+//     if(max>max1){
+//         console.log(max);
+//     }else{
+//         console.log(max1);
+//     }
+
+        
+
+// const input=require("readline-sync");
+// let y=input.questionInt("entera")
+// if(y%4==0){console.log("leap year")
+// }
+
+// if(y%100!==0){
+//     console.log("leap year")
+// }
+
+
+// else if(y%400==0)
+//    { console.log("leap year")}
+
+    
+// else{
+
+//    console.log("not leap year")
+// }
+
+
+// const input=require("readline-sync");
+// let a=input.questionInt("enter a");
+// let b=input.questionInt("enterb");
+// let c=input.questionInt("enterc");
+// if(a>b){
+//     max=a;
+//     min=b;
+// }else{
+//     max=b;
+//     min=a;
+// }
+//     if(max>c){
+//         max1=a;
+//         min1=c;
+//     }else{
+//         max1=c;
+//         min1=a;
+//     }
+//     if(min1>min){
+//         console.log(" second min1" );
+//     }else{
+//         console.log("min");
+//     }
+
+// let bs=input.questionInt("enter a number");
+// if(bs<=10000){
+//     hra=bs*20/100;
+//     da=bs*
+//     80/100;
+//     gs=bs+hra+da;
+// }else{
+// if(bs>20000){
+//     hra=bs*30/100;const input=require("readline-sync");
+
+//     da=bs*90/100;
+//     gs=bs+hra+da;
+// }else{
+//     hra=bs*25/100;
+//     da=bs*90/100;
+//     gs=bs+hra+da;
+// }
+// }
+// console.log(gs)
+
+
+// let b,s,t;
+// if(u<50){
+//     b=u*0.50;
+//     s=b*20/100;const input=require("readline-sync");
+
+//     t=b+s;}
+
+//     if(u<150){
+//         b=(u*0.50)+(u-50)*0.75;
+//         s=b*20/100;
+//         t=b+s;}
+    
+    
+//         if(u<250){let u=input.questionInt("enteru");
+
+//             b=100+(u-150)*1.20;
+//             s=b*20/100;
+//             t=b+s;}
+        
+//     else{
+//            b=20.+(u-250)*1.50;
+//             s=b*20/100;
+//             t=b+s;
+//         }
+//             console.log(t)
+
+// const input=require("readline-sync");
+// let a=input.questionInt("enter a");
+
+// let b=input.questionInt("enter b");
+// let c=input.questionInt("enter c");
+// let d=input.questionInt("enter d");
+// if(a>b){
+//   max=a;
+//   min=b;const input=require("readline-sync");
+
+// }
+// else{
+//   max=b;
+//   min=a;
+// }
+// if(c>d){
+//   max1=c;
+//   min1=d;
+// }
+// else{
+//   max1=d;
+//   min1=c;
+// }
+// if(max>max1){
+//   max2=max;
+//   min2=max1;
+// }
+// else{
+//   max2=max1;
+//   min2=max
+// }
+// if(min>min1){
+//   max3=min;
+//   min3=min1;
+// }
+// else{
+
+//   max3=min1;
+//   min3=min;
+// }
+// if(max2>max3){
+//   max4=max2;
+//   min4=max3;
+// }
+// else{
+//   max4=max4;
+//   min4=max2;
+// }
+// if(min2>min3){
+//   console.log(min2);
+// }
+// else{
+//   console.log(min3);
+// }
+  
+// const input=require("readline-sync");
+// let a=input.questionInt("entera");
+// let b=input.questionInt("enterb");
+// let c=input.questionInt("enterc");
+// let d=input.questionInt("enter d");
+// let e=input.questionInt("enter e");
+// let f=input.questionInt("enter f");
+// let g=input.questionInt("enterg");
+// let h=input.questionInt("enter h");
+// if(a>b){
+//   max=a;
+//   min=b;
+// }
+// else{
+// max=b;
+// min=a;
+// }
+// if(c>d){
+//   max1=c;
+//   min1=d;
+// }
+// else{
+//   max1=d;
+//   min1=c;
+// }
+// if(e>f){
+//   max2=e;
+//   min2=f;
+// }
+// else{
+//   max2=f;
+//   min2=e;
+// }
+// if(g>h){
+//   max3=g;
+//   min3=h;
+// }
+// else{
+//   max3=h;
+//   min3=g;
+// }
+// if(max>max1){
+//   max4=max;
+//   min4=max1;
+// }
+// else{
+//   max4=max1;
+//   min4=max;
+// }
+// if(max2>max3){
+//   max5=max2;
+//   min5=max3;
+// }
+// else{
+//   max5=max3;
+//   min5=max2;
+// }
+// if(max4>max5){
+// max6=max4;
+// min6=max5;
+// }
+// else{
+//   max6=max5;
+//   min6=max4;
+// }
+// if(min>min1){
+//   max7=min;
+//   min7=min1;
+// }
+// else{
+//   max7=min1;
+//   min7=min;
+// }
+// if(min2>min3){
+//   max8=min2;
+//   min8=min3;
+// }
+// else{
+//   max8=min3;
+//   min8=min2;
+// }
+// if(max7>max8){
+//   max9=max7;
+//   min9=max8;
+// }
+// else{
+//   max9=max8;
+//   min9=max7;
+// }
+// if(max9>min6){
+//   console.log(max9);
+// }
+// else{
+//   console.log(min6);
+// }
+
+
+// const input=require("readline-sync");
+// var a=input.questionInt("enter a number");
+// var i=1;
+// var c=0;
+// while(i<=a)
+// {
+//   if(a%i==0){
+//     c=c+1;
+//   }
+//   i=i+1;
+// }
+// if(c==2)
+// {
+//   console.log("primenumber");
+// }else
+// {console.log("not prime number");
+// }
+
+
+
+
+
+
+
+
+  
+  
+  
+
+
+
+  
+
+
+
+ 
+
+    
