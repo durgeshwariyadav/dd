3262210670b192680f9f0250c8e74f3407827b74
durgeshwariyadav <163391069+durgeshwariyadav@users.noreply.github.com>
2024-03-14 14:38:29 +0530

    Add files via upload

---
+++ loop.js
@@ -0,0 +1,228 @@
+// const input=require("readline-sync");
+// let n=input.question("entear  the numbar");
+// let a=input.question("entar the  numbar");
+// foconst input=require("readline-sync");
+// r(i=1;i<n;i++){
+// const input =require("readline-sync");
+// console.log(a)
+// }
+// const input=require("readline-sync");
+// let n=input.questionInt("enter n ");
+// let i=2;
+// while(i<n){
+// if(i%2==0){
+//     console.log(i);
+//     i=i+1
+// }else{
+//     i = i+1
+// }
+// }
+        
+// const  input=require("readline-sync") ;
+// let n=input.questionInt("enter n") ;
+// let i=1;
+// sum=0;
+// while(i<n){
+//     if(i%2!==0){
+//         sum=sum+i
+//         };
+//         i=i+1
+//     }
+//     {
+    
+//     console.log(sum);
+// }
+
+// const input=require("readline-sync");
+// let m = input.questionInt("enter m ");
+// let n = input.questionInt("enter n ");
+// let i = 2;
+// let sum = 0;
+// while(i<=n){
+// sum=sum+i;
+// i =i+1;
+// }
+// console.log(sum);
+
+// const input=require("readline-sync");
+// let N=input .questionInt("enter n");
+// let i =1;
+// let sum=0;
+// while(i<=n){
+//   sum=sum+1/i
+//   i=i+1
+// }
+// console.log(sum);
+
+
+
+
+
+// const input=require("readline-sync");
+// let a=input.questionInt("enter a number");
+// let i=0;
+// let c=0;
+// while(n=0){
+//     r=n%10;
+//     n=math.floor(n/10);
+//     c=c+1;
+// }
+// console.log(c);
+
+// const input=require("readline-sync");
+// let a =input.questionInt("enter a number");
+// let c= 0;
+// let n;
+// while(a>0){
+//     n=a%10;
+//     c=c+1;
+//     a=Math.floor(a/10);
+// }
+// console.log("number of digits",c);
+    
+//     const input=require("readline-sync");
+//     let a=input.questionInt("enter a number");
+//     let reversednum=0
+//     while(a>0){
+//         let b=a%10;
+//      reversednum =(reversednum*10)+b
+//     a=Math.floor(a/10);
+// }
+// console.log(reversednum);
+
+//    const  input=require("readline-sync");
+//    let n = input.questionInt("enter  n:");
+//    let i=1;
+//    let sum=0;
+//    while(i<=n){
+//     let r=n%10;
+//     sum+=r;
+//     n=Math.floor(n/10);
+//    }
+//    console.log(sum);
+
+//    const input=require("readline-sync");
+//    let n=input.questionInt("enter the number");
+//    let i=1;
+//    let sum=0;
+//    while(i<n){
+
+// if(n%i==0)
+// {
+//     i=i+1;
+// }
+// else{
+//     sum=sum+i;
+//     i++;
+// }
+// if(sum==0)
+// {
+//     console.log("perfect number");
+// }else console.log("not perfect number");
+//    }
+
+// const input=require("readline-sync");
+// let a=input.questionInt("enter a");
+// let b=input.questionInt("enter b ");
+// p=a*b;
+// while(a%b!==0){
+//     var k=a%b;
+//     a=b;
+//     b=k;
+// }
+// console.log(b);
+// console.log(p/b);
+   
+  // const input=require("readline-sync") ;
+  // let n=input.questionInt("enter n");
+  // let i=1;
+  // while(i<=n)
+  // {
+  //   while(n%i==0)
+  //   {
+  //     console.log(i);
+  //     i++;
+  //   }
+  //   i++;
+  // }
+      const input=require("readline-sync");
+      let n=input.questionInt("enter n");
+      let i=1;
+      while(i<=n){
+        j=1;
+        if(j<=i);
+        console.log
+
+
+
+
+
+
+
+
+
+
+        
+
+
+
+      
+    
+    
+    
+    
+    
+  
+    
+        
+  
+    
+
+
+
+
+
+
+
+
+
+
+
+   
+    
+    
+
+   
+   
+
+
+
+   
+
+
+
+
+
+
+
+    
+
+
+
+    
+
+    
+    
+    
+    
+
+
+    
+    
+    
+    
+    
+
+    
+
+ 
---
+++ variable.js
@@ -0,0 +1,73 @@
+// const input=require("readline-sync");
+// let a=input.questionInt("enter brith date");
+// let b=input.question("enterbrith month");
+// let c=input.question("brith year");
+// let d=a;
+// let m=b;
+// let y=c;
+// console.log(d,m,y)
+
+const { questionInt } = require("readline-sync");
+
+// const input=require("readline-sync");
+// let n=input.questionInt("enter the number");
+// console.log(n);
+
+// const input=require("readline-sync");
+// let a=input.questionInt("enter number a");
+// let b=input.questionInt("enter numberb");
+// console.log(b);
+// console.log(a);
+
+
+
+// const input =require("readline-sync");
+// let a=input.questionInt("enter a");
+// let b=input.questionInt("enterb");
+// let p,q,r,s,t;undefined;
+// p=a+b
+// q=a-b
+// r=a*b
+// s=a/b
+// t= Math.floor(a/b)
+// u=a%b
+// console.log(p,q,r,s,t,u)
+
+// const input=require("readline-sync");
+// let a=input.questionInt("entera");
+// let b=input.questionInt("enterb");
+// let p,r;
+// p=Math.floor(a/b);
+// r=a%b;
+// console.log(p,r)
+
+// const input=require("readline-sync");
+
+// let a=input.questionInt("entera");
+// d=a%10;
+// console.log(d);
+
+// const input=require("readline-sync");
+// let c=input.questionInt("enterc");
+// f=9/5*c+32 ;
+// console.log(f);
+
+// const input=require("readline-sync");
+// let a= input.questionInt("enter a");
+// let b=input.questionInt("enterb");
+// console.log(b);
+// console.log(a);
+
+
+// const input=require("readline-sync");
+// let a=input.questionInt("enter the number");
+// let b=input.questionInt("enter the number");
+
+//     c=Math.floor(a/b)*b;
+
+//     console.log(c);
+
+
+
+
+
